6926cc20ff00deaf5cfaf24f4d9340ebc9f656bc
S401-07 <S401-07@S401-07>
2016-08-11 16:00:39 +0900

    20160811 페이징 처리 중..

--- src/kr/ac/sungkyul/mysite/dao/BoardDao.java
+++ src/kr/ac/sungkyul/mysite/dao/BoardDao.java
@@ -154,10 +154,10 @@ public class BoardDao {
 		return list;
 	}
 
-	public void delete(Long no){
+	public void delete(Long no) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
-		
+
 		try {
 			conn = getConnection();
 
@@ -182,9 +182,9 @@ public class BoardDao {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
-	
+
 	public void update(BoardVo vo) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -220,8 +220,8 @@ public class BoardDao {
 			}
 		}
 	}
-	
-	public void viewCount(Long no){
+
+	public void viewCount(Long no) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 
@@ -229,11 +229,11 @@ public class BoardDao {
 			conn = getConnection();
 
 			String sql = "update board set view_count=view_count+1 where no=?";
-			
+
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setLong(1, no);
-			
+
 			pstmt.executeUpdate();
 
 		} catch (SQLException e) {
@@ -250,5 +250,82 @@ public class BoardDao {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	public int totalBoard() {
+		int total = 0;
+
+		Connection conn = null;
+		Statement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = getConnection();
+			pstmt = conn.createStatement();
+			String sql = "select count(*) from board";
+
+			rs = pstmt.executeQuery(sql);
+			if(rs.next()){
+				total = rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return total;
+	}
+	public List<BoardVo> getList(Integer page, Integer row){
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<BoardVo> list = new ArrayList<BoardVo>();
+		try {
+			conn = getConnection();
+			
+			String sql = "select no, title, content, reg_date, view_count, name, user_no, depth "
+					+ "from (select a.*, ROWNUM as rn "
+					+ "from (select b.no, b.title, b.content, to_char(b.reg_date, 'yyyy-mm-dd pm hh12:mi:ss') as reg_date, "
+					+ "b.view_count, b.group_no, b.order_no, b.depth, u.NAME, b.user_no from board b, users u "
+					+ "where b.USER_NO = u.NO order by group_no desc, order_no asc) a "
+					+ "where a.title like '%%') where rn >= ? and rn <= ? order by group_no desc, order_no asc";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()){
+				Long no = rs.getLong(1);
+				String title = rs.getString(2);
+				String content = rs.getString(3);
+				String regDate = rs.getString(4);
+				Long viewCount = rs.getLong(5);
+				String name = rs.getString(6);
+				Long userNo = rs.getLong(7);
+				Long depth = rs.getLong(8);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
 	}
 }
